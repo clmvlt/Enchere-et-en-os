32b91a9f3061e7f59b334c7a6f3b6d200d1c153a
Clement Veillet <clementveillet@gmail.com>
2022-12-08 11:02:05 +0100

    commit btn retour enchere flash

--- ProjetAP/Vues/VuesEncheres/EncheresFlashsVue.xaml.cs
+++ ProjetAP/Vues/VuesEncheres/EncheresFlashsVue.xaml.cs
@@ -1,3 +1,4 @@
+using ProjetAP.Modeles;
 using ProjetAP.VuesModeles.VuesModelesEncheres;
 
 namespace ProjetAP.Vues.VuesEncheres;
@@ -9,5 +10,17 @@ public partial class EncheresFlashsVue : ContentPage
 	{
 		InitializeComponent();
 		vueModele = new EncheresFlashsVueModele();
+    }
+
+    private void btnViewEnchere_Clicked(object sender, EventArgs e)
+    {
+        Button btn = sender as Button;
+        Enchere selectedEnchere = btn.BindingContext as Enchere;
+        App.Current.MainPage = new UneEnchereClassiqueVue(selectedEnchere);
+    }
+
+    private void btnRetour_Clicked(object sender, EventArgs e)
+    {
+        App.Current.MainPage = new AccueilVue();
     }
 }
--- ProjetAP/VuesModeles/VuesModelesEncheres/EncheresFlashsVueModele.cs
+++ ProjetAP/VuesModeles/VuesModelesEncheres/EncheresFlashsVueModele.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ProjetAP.Modeles;
+using ProjetAP.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +12,29 @@ namespace ProjetAP.VuesModeles.VuesModelesEncheres
     public class EncheresFlashsVueModele : BaseVueModele
     {
         #region Attributes
+        private ObservableCollection<Enchere> _encheresFlashs;
         #endregion
 
         #region Constructeurs
+        public EncheresFlashsVueModele()
+        {
+            AfficherLesEncheres();
+        }
         #endregion
 
         #region Properties (Getters/Setters)
+        public ObservableCollection<Enchere> EncheresFlash
+        {
+            get => _encheresFlashs;
+            set => SetProperty(ref _encheresFlashs, value);
+        }
         #endregion
 
         #region Méthodes
+        public async void AfficherLesEncheres()
+        {
+            EncheresFlash = await APIEnchere.GetEncheresEnCours(3);
+        }
         #endregion
     }
 }
